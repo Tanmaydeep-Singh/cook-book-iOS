3503783a469a8d9c82503c6fd6fb4cee5091a412
Tanmaydeep-Singh <simarsing007@gmail.com>
2026-01-10 11:01:48 +0530

    Dev: Cook-book base structure setup

---
+++ cookbook/App/cookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  cookbookApp.swift
+//  cookbook
+//
+//  Created by tanmaydeep on 31/01/26.
+//
+
+import SwiftUI
+
+@main
+struct cookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
